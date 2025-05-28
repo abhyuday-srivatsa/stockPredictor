fbb613ec5e6eba04ee12d08ceebd2c9b02073e05
JaredKimia1 <jaredkimia@gmail.com>
2025-05-28 17:31:47 -0400

    more data cleaning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import nltk
+import re
 nltk.download('punkt')
 from nltk import sent_tokenize, word_tokenize
 from nltk.stem.wordnet import WordNetLemmatizer
@@ -8,6 +9,7 @@ from nltk.corpus import stopwords
 nltk.download('stopwords', quiet=True)
 
 STOPWORD_SET = set(stopwords.words('english'))
+JUNKWORD_SET = set(["'s", "'", " ",'"', "``", "''", ",", ".", "!", "?", ";", ":", "-", "_", "(", ")", "[", "]", "{", "}"])
 
 def remove_stopwords(sentence):
     filtered = []
@@ -23,18 +25,28 @@ def stem(sentence):
         stemmed.append(stemmer.stem(word))
     return stemmed
 
+def remove_junk_characters(sentence):
+    year_pattern = re.compile(r'^(19|20)\d{2}$')
+    filtered = []
+    for word in sentence:
+        if word not in JUNKWORD_SET and not year_pattern.match(word):
+            filtered.append(word)
+    return filtered
+
+
 def preprocess_data(df):
     sentences = df.Sentence.values
     lowercased = [sentence.lower() for sentence in sentences]
     tokenized = [sentence.split() for sentence in lowercased]
     cleaned = [remove_stopwords(sentence) for sentence in tokenized]
     stemmed = [stem(sentence) for sentence in cleaned]
+    junk_cleaned = [remove_junk_characters((sentence)) for sentence in stemmed]
     #lemmatization
-    df['Sentence'] = stemmed
+    df['Sentence'] = junk_cleaned
     return df
 
 train_data = pd.read_csv("finance_train.csv")
 test_data = pd.read_csv("finance_test.csv")
 
 train_data = preprocess_data(train_data)
-print(train_data.head())
+print(train_data)
