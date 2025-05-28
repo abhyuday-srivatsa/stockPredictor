ff134d7c1902506f9ea5d38ea9261f7d3f10652f
AbhyudaySrivatsa <abhyuday.srivatsa@gmail.com>
2025-05-28 14:50:37 -0700

    training model

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import nltk
 import re
-nltk.download('punkt')
+import sklearn
+
+nltk.download('punkt', quiet=True)
 from nltk import sent_tokenize, word_tokenize
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.stem.porter import *
 from nltk.corpus import stopwords
 nltk.download('stopwords', quiet=True)
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
 
 STOPWORD_SET = set(stopwords.words('english'))
 JUNKWORD_SET = set(["'s", "'", " ",'"', "``", "''", ",", ".", "!", "?", ";", ":", "-", "_", "(", ")", "[", "]", "{", "}"])
@@ -41,12 +47,28 @@ def preprocess_data(df):
     cleaned = [remove_stopwords(sentence) for sentence in tokenized]
     stemmed = [stem(sentence) for sentence in cleaned]
     junk_cleaned = [remove_junk_characters((sentence)) for sentence in stemmed]
-    #lemmatization
+
     df['Sentence'] = junk_cleaned
-    return df
+    return df['Sentence'], df['Label']
 
 train_data = pd.read_csv("finance_train.csv")
 test_data = pd.read_csv("finance_test.csv")
 
-train_data = preprocess_data(train_data)
-print(train_data)
+train_sentences, train_labels = preprocess_data(train_data)
+
+
+def train_model(train_sentences, train_labels):
+    train_sentences = [" ".join(t) for t in train_sentences]
+    train_labels = [l for l in train_labels]
+
+    vectorizer = CountVectorizer()
+
+    vectorizer.fit(train_sentences)
+    train_vect = vectorizer.transform(train_sentences)
+
+    model = LogisticRegression()
+
+    model.fit(train_vect, train_labels)
+
+    return model, vectorizer
+
